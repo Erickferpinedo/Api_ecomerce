8037c25072f2d41504f4c981e7675d65e2a8bc6f
Erickferpinedo <erickfprogrammer@gmail.com>
2024-09-20 14:30:12 -0400

    all done with the routes and now i will be adding it to the server js and i will test all the routes

--- routes/pedidoRoutes.js
+++ routes/pedidoRoutes.js
@@ -1,2 +1,14 @@
-import * as pedidocontroller from "../controllers/pedidocontroller.js"
+import express from "express";
+import * as pedidocontroller from "../controllers/pedidocontroller.js";
+
+const router = express.Router();
+
+router.get('/pedido', pedidocontroller.getAll);
+router.get('/pedido/:id', pedidocontroller.getById);
+router.post('/pedido', pedidocontroller.create);
+router.put('/pedido/:id', pedidocontroller.update);
+router.delete('/pedido/:id', pedidocontroller.deleted);
+
+export default router;
+
 
