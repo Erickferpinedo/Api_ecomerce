320938ac1192addac5df16a5f703344a2b783c00
Erickferpinedo <erickfprogrammer@gmail.com>
2024-09-20 14:36:34 -0400

    i was fixing a typo error that was not even letting me start the program correctly but now the program is starting and running. i will be checking now all the routes with postman

--- routes/pedidoRoutes.js
+++ routes/pedidoRoutes.js
@@ -7,7 +7,7 @@ router.get('/pedido', pedidocontroller.getAll);
 router.get('/pedido/:id', pedidocontroller.getById);
 router.post('/pedido', pedidocontroller.create);
 router.put('/pedido/:id', pedidocontroller.update);
-router.delete('/pedido/:id', pedidocontroller.deleted);
+router.delete('/pedido/:id', pedidocontroller.softDelete);
 
 export default router;
 
